79d51c8f790890fa43718bf7a03bd5e272438987
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-06-11 19:25:33 +0530

    change in js code

--- sricpt.js
+++ sricpt.js
@@ -1,4 +1,5 @@
 const headtxt = document.getElementById('quote');
+const writer = document.getElementById('writer');
 const api_url = "https://api.quotable.io/random";
 
 async function quote(url)
@@ -7,5 +8,6 @@ async function quote(url)
     const data = await response.json();
     console.log(data);
     headtxt.innerText= data.content;
+    writer.innerText = data.author
 }
 
