7ca6c07ce00d516a91d2f44ce06896499094e02e
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-06-11 19:19:27 +0530

    add js code and api

--- sricpt.js
+++ sricpt.js
@@ -1,1 +1,11 @@
-let headtxt = document.getElementsByClassName
+const headtxt = document.getElementById('quote');
+const api_url = "https://api.quotable.io/random";
+
+async function quote(url)
+{
+    const response =await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    headtxt.innerText= data.content;
+}
+
