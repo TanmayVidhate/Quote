043daf3ae0823279672273c8be5e5cea2075b5a5
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-06-11 19:46:18 +0530

    add chnage

--- sricpt.js
+++ sricpt.js
@@ -4,7 +4,7 @@ const api_url = "https://api.quotable.io/random";
 
 async function quote(url)
 {
-    const response =await fetch(url);
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     headtxt.innerText= data.content;
